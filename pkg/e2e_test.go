22ac38f269cc361df3ea9d5115e0bcb18a425b63
Tiger Kaovilai <tkaovila@redhat.com>
2022-06-28 12:12:53 -0400

    add e2e test scenario

--- pkg/e2e_test.go
+++ pkg/e2e_test.go
@@ -8,11 +8,16 @@ import (
 
 	"github.com/containers/image/v5/copy"
 	"github.com/containers/image/v5/docker"
+	"github.com/containers/image/v5/image"
+	"github.com/containers/image/v5/manifest"
 	"github.com/containers/image/v5/signature"
+	"github.com/containers/image/v5/transports"
 	"github.com/containers/image/v5/types"
 	"github.com/distribution/distribution/v3/uuid"
 	"github.com/kaovilai/udistribution/pkg/image/udistribution"
+	"github.com/kaovilai/udistribution/pkg/internal/image/imagesource"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 /*
@@ -98,7 +103,93 @@ func TestE2e(t *testing.T) {
 	// if err != nil {
 	// 	t.Errorf("failed to delete image: %v", err)
 	// }
-
+	
+	// test that udistributionReference when used as source can call ConfigBlob() which is used during velero restore
+	publicRawSource, err := destRef.NewImageSource(context.Background(), ctx)
+	if err != nil {
+		t.Error(errors.Wrapf(err, "initializing source %s", transports.ImageName(destRef)))
+	}
+	rawSource := imagesource.FromPublic(publicRawSource)
+	defer func() {
+		if err := rawSource.Close(); err != nil {
+			t.Errorf(" (src: %v)", err)
+		}
+	}()
+	unparsedToplevel := image.UnparsedInstance(rawSource, nil)
+	// Parse the list and get a copy of the original value after it's re-encoded.
+	manifestList, manifestType, err := unparsedToplevel.Manifest(context.Background())
+	if err != nil {
+		t.Error(errors.Wrapf(err, "reading manifest list"))
+	}
+	originalList, err := manifest.ListFromBlob(manifestList, manifestType)
+	if err != nil {
+		t.Error(errors.Wrapf(err, "parsing manifest list %q", string(manifestList)))
+	}
+	options := copy.Options{
+		SourceCtx:      ctx,
+		DestinationCtx: ctx,
+	}
+	updatedList := originalList.Clone()
+	instanceDigests := updatedList.Instances()
+	imagesToCopy := len(instanceDigests)
+	if options.ImageListSelection == copy.CopySpecificImages {
+		imagesToCopy = len(options.Instances)
+	}
+	updates := make([]manifest.ListUpdate, len(instanceDigests))
+	instancesCopied := 0
+	for i, instanceDigest := range instanceDigests {
+		if options.ImageListSelection == copy.CopySpecificImages {
+			skip := true
+			for _, instance := range options.Instances {
+				if instance == instanceDigest {
+					skip = false
+					break
+				}
+			}
+			if skip {
+				update, err := updatedList.Instance(instanceDigest)
+				if err != nil {
+					t.Error(err)
+				}
+				logrus.Debugf("Skipping instance %s (%d/%d)", instanceDigest, i+1, len(instanceDigests))
+				// Record the digest/size/type of the manifest that we didn't copy.
+				updates[i] = update
+				continue
+			}
+		}
+		logrus.Debugf("Copying instance %s (%d/%d)", instanceDigest, i+1, len(instanceDigests))
+		logrus.Printf("Copying image %s (%d/%d)\n", instanceDigest, instancesCopied+1, imagesToCopy)
+		unparsedInstance := image.UnparsedInstance(rawSource, &instanceDigest)
+		// updatedManifest, updatedManifestType, updatedManifestDigest, err := c.copyOneImage(ctx, policyContext, options, unparsedToplevel, unparsedInstance, &instanceDigest)
+		// if err != nil {
+		// 	return nil, err
+		// }
+		instancesCopied++
+		// // Record the result of a possible conversion here.
+		// update := manifest.ListUpdate{
+		// 	Digest:    updatedManifestDigest,
+		// 	Size:      int64(len(updatedManifest)),
+		// 	MediaType: updatedManifestType,
+		// }
+		// updates[i] = update
+		// targetInstance := instanceDigest
+		// Now reset the digest/size/types of the manifests in the list to account for any conversions that we made.
+		// if err = updatedList.UpdateInstances(updates); err != nil {
+		// 	t.Error(errors.Wrapf(err, "updating manifest list"))
+		// }
+		// manifestBytes, retManifestDigest, err := ic.copyUpdatedConfigAndManifest(ctx, targetInstance)
+		src, err := image.FromUnparsedImage(context.Background(), options.SourceCtx, unparsedInstance)
+		if err != nil {
+			t.Error(errors.Wrapf(err, "creating source image"))
+		}
+		pendingImage := src
+		pendingImage.ConfigInfo()
+		configBlob, err := src.ConfigBlob(context.Background())
+		if err != nil {
+			t.Error(errors.Wrapf(err, "getting config blob"))
+		}
+		logrus.Infof("config blob: %v", configBlob)
+	}
 }
 
 func getPolicyContext() (*signature.PolicyContext, error) {
